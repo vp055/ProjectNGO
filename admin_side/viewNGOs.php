2281c0b9264072ea05709dfd25038ddb0a3563e1
Tanuj Srivastava <Tanuj@Tanujs-MacBook-Air.local>
2015-11-07 23:40:22 +0530

    navbar in viewngo

--- admin_side/viewNGOs.php
+++ admin_side/viewNGOs.php
@@ -4,22 +4,51 @@ include('session.php');
 	
 <html>
 
-<head>
-    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.min.css">
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.min.css">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
 
-</head>
-<body>
+    </head>
+    <body>
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="navbar-brand page-scroll" href="#page-top">Project NGO</div>
+                </div>
 
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="#about"><?php echo $login_session; ?></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="admin.php">Admin Portal</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="logout.php">Sign Out</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="root.php">Root Portal</a>
+                        </li>
+                    </ul>
+                </div>
+            <!-- /.navbar-collapse -->
+            </div>
+        <!-- /.container-fluid -->
+        </nav>
 	
-		
-		
-  <h1> Welcome <?php echo $login_session;?></h1> <br> 
-	<h2><?php echo "<a href='logout.php'> Logout</a> "; ?></h2><br><br>
 
 	
 			
-	REGISTERED NGOs<br><br>
+        REGISTERED NGOs<br><br>
 	
 <?php
  
